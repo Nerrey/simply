ea6f83829907d3016933213821a78a7f13f13575
Nik <kolud@bk.ru>
2015-10-05 15:43:27 +0300

    fix

---
+++ config/deploy/locum.rb
@@ -0,0 +1,2 @@
+role :app, %w(hosting_nerrey@phosphorus.locum.ru)
+set :ssh_options, forward_agent: true
---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,9 @@
+# config/initializers/secret_token.rb
+
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Simply::Application.config.secret_token = '5c01dd65b089471792033cc2385a95a7ecd2d6f2101a9851963081e05b403d49cca91c41409733bbb4a08633d884d50a256653ce547420e026c324900e90d5d2'

a19d0893bfd0cb499903b8be753569ca428d3980
Nik <kolud@bk.ru>
2015-10-06 15:30:50 +0300

    fix deploy + some fixes

--- config/boot.rb
+++ config/boot.rb
@@ -1,3 +1,3 @@
-ENV['BUNDLE_GEMFILE'] = "/home/hosting_nerrey/projects/simply/releases/current/Gemfile"
+ENV['BUNDLE_GEMFILE'] ||= File.expand_path('../../Gemfile', __FILE__)
 
 require 'bundler/setup' # Set up gems listed in the Gemfile.
